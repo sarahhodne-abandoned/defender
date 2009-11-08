7b45711eab13601d00cee4e3e83ba60bfd9eca18
Henrik Hodne <henrik.hodne@gmail.com>
2009-11-08 22:32:53 +0100

    Updated tests to not use Defensio, but Mocha instead

--- spec/defender_spec.rb
+++ spec/defender_spec.rb
@@ -1,6 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Defender" do
+  before(:each) do
+    @defender = Defender.new(:api_key => "validkey", :owner_url => "validurl")
+  end
+
   it "should raise a StandardError if a method fails" do
     lambda do
       Defender.raise_if_error({
@@ -11,61 +15,102 @@ describe "Defender" do
   end
   
   it "should return the correct URL for any given action" do
-    d = Defender.new(:api_key => "key1234")
-    d.instance_eval do
+    @defender.instance_eval do
       url("foobar")
-    end.should == "http://api.defensio.com/blog/#{Defender::API_VERSION}/foobar/key1234.yaml"
+    end.should == "http://api.defensio.com/blog/#{Defender::API_VERSION}/foobar/validkey.yaml"
   end
   
   it "should correctly identify a valid API key" do
-    d = Defender.new(:api_key => ENV["API_KEY"], :owner_url => ENV["API_OWNER_URL"])
-    d.valid_key?.should be_true
-  end if ENV["API_KEY"]
+    @defender.stubs(:call_action).with("validate-key").returns(
+      {"status" => "success", "message" => ""}
+    )
+    @defender.valid_key?.should be_true
+  end
   
   it "should correctly identify an invalid API key" do
-    d = Defender.new(:api_key => "key1234", :owner_url => ENV["API_OWNER_URL"])
-    d.valid_key?.should be_false
+    @defender.stubs(:call_action).with("validate-key").returns(
+      {"status" => "fail", "message" => "Invalid key"}
+    )
+    @defender.valid_key?.should be_false
   end
   
   it "should correctly identify a spammy comment" do
-    d = Defender.new(:api_key => ENV["API_KEY"], :owner_url => ENV["API_OWNER_URL"])
-    
-    d.audit_comment(
+    @defender.
+      stubs(:call_action).
+      with('audit-comment', {
+        "user-ip" => "127.0.0.1",
+        "article-date" => Time.now.strftime("%Y/%m/%d"),
+        "comment-author" => "Henrik Hodne",
+        "comment-type" => "comment",
+        "test-force" => "spam,0.5000",
+      }).
+      returns(
+      {"signature" => "abc123", "spam" => true, "spaminess" => 0.5}
+    )
+    @defender.audit_comment(
       :user_ip => "127.0.0.1",
       :article_date => Time.now,
       :comment_author => "Henrik Hodne",
       :comment_type => "comment",
       :test_force => "spam,0.5000"
     ).spam?.should be_true
-  end if ENV["API_KEY"]
+  end
   
   it "should correctly identify a meaty comment" do
-    d = Defender.new(:api_key => ENV["API_KEY"], :owner_url => ENV["API_OWNER_URL"])
-    
-    d.audit_comment(
+    @defender.
+      stubs(:call_action).
+      with('audit-comment', {
+        "user-ip" => "127.0.0.1",
+        "article-date" => Time.now.strftime("%Y/%m/%d"),
+        "comment-author" => "Henrik Hodne",
+        "comment-type" => "comment",
+        "test-force" => "ham,0.1000",
+      }).
+      returns(
+      {"signature" => "abc123", "spam" => false, "spaminess" => 0.1}
+    )
+    @defender.audit_comment(
       :user_ip => "127.0.0.1",
       :article_date => Time.now,
       :comment_author => "Henrik Hodne",
       :comment_type => "comment",
       :test_force => "ham,0.1000"
     ).spam?.should be_false
-  end if ENV["API_KEY"]
+  end
   
   it "should correctly set the spaminess" do
-    d = Defender.new(:api_key => ENV["API_KEY"], :owner_url => ENV["API_OWNER_URL"])
-    
-    d.audit_comment(
+    @defender.
+      stubs(:call_action).
+      with('audit-comment', {
+        "user-ip" => "127.0.0.1",
+        "article-date" => Time.now.strftime("%Y/%m/%d"),
+        "comment-author" => "Henrik Hodne",
+        "comment-type" => "comment",
+        "test-force" => "spam,0.5000",
+      }).
+      returns(
+      {"signature" => "abc123", "spam" => true, "spaminess" => 0.5}
+    )
+    @defender.audit_comment(
       :user_ip => "127.0.0.1",
       :article_date => Time.now,
       :comment_author => "Henrik Hodne",
       :comment_type => "comment",
       :test_force => "spam,0.5000"
     ).spaminess.should == 0.5
-  end if ENV["API_KEY"]
+  end
   
   it "should fail without valid API credentials" do
-    d = Defender.new(:api_key => "key1234", :owner_url => "http://google.com")
-    
+    @defender.
+      stubs(:call_action).
+      with('audit-comment', {
+        "user-ip" => "127.0.0.1",
+        "article-date" => Time.now.strftime("%Y/%m/%d"),
+        "comment-author" => "Henrik Hodne",
+        "comment-type" => "comment",
+        "test-force" => "ham,0.1000",
+      }).
+      raises(StandardError)
     lambda {
       d.audit_comment(
         :user_ip => "127.0.0.1",
@@ -74,6 +119,6 @@ describe "Defender" do
         :comment_type => "comment",
         :test_force => "ham,0.1000"
       )
-    }.should raise_error(StandardError, "Authentication failed. Please verify your key/owner-url combination.")
+    }.should raise_error(StandardError)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'defender'
 require 'spec'
 require 'spec/autorun'
+require 'mocha'
 
 Spec::Runner.configure do |config|
   
