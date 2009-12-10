a175d2b0e83ea0b622f6a771e39d7a2ab5432778
Henrik Hodne <henrik.hodne@binaryhex.com>
2009-12-10 20:00:23 +0100

    Rewrote tests for the 2.0 API

--- spec/defender_spec.rb
+++ spec/defender_spec.rb
@@ -1,207 +1,58 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Defender" do
-  before(:each) do
-    @defender = Defender.new(:api_key => "validkey", :owner_url => "validurl")
-  end
-
-  it "should raise a StandardError if a method fails" do
-    lambda do
-      Defender.raise_if_error({
-        "status" => "fail",
-        "message" => "Failed!"
-       })
-    end.should raise_error(StandardError, "Failed!")
-  end
-  
-  it "should return the correct URL for any given action" do
-    @defender.instance_eval do
-      url("foobar")
-    end.should == "http://api.defensio.com/blog/#{Defender::API_VERSION}/foobar/validkey.yaml"
-  end
-  
-  it "should correctly identify a valid API key" do
-    @defender.stubs(:call_action).with("validate-key").returns(
-      {"status" => "success", "message" => ""}
-    )
-    @defender.valid_key?.should be_true
-  end
-  
-  it "should correctly identify an invalid API key" do
-    @defender.stubs(:call_action).with("validate-key").returns(
-      {"status" => "fail", "message" => "Invalid key"}
-    )
-    @defender.valid_key?.should be_false
-  end
-  
-  it "should correctly identify a spammy comment" do
-    @defender.
-      stubs(:call_action).
-      with('audit-comment', {
-        "user-ip" => "127.0.0.1",
-        "article-date" => Time.now.strftime("%Y/%m/%d"),
-        "comment-author" => "Henrik Hodne",
-        "comment-type" => "comment",
-        "test-force" => "spam,0.5000",
-      }).
-      returns(
-      {"signature" => "abc123", "spam" => true, "spaminess" => 0.5}
-    )
-    @defender.audit_comment(
-      :user_ip => "127.0.0.1",
-      :article_date => Time.now,
-      :comment_author => "Henrik Hodne",
-      :comment_type => "comment",
-      :test_force => "spam,0.5000"
-    ).spam?.should be_true
-  end
-  
-  it "should correctly identify a meaty comment" do
-    @defender.
-      stubs(:call_action).
-      with('audit-comment', {
-        "user-ip" => "127.0.0.1",
-        "article-date" => Time.now.strftime("%Y/%m/%d"),
-        "comment-author" => "Henrik Hodne",
-        "comment-type" => "comment",
-        "test-force" => "ham,0.1000",
-      }).
-      returns(
-      {"signature" => "abc123", "spam" => false, "spaminess" => 0.1}
-    )
-    @defender.audit_comment(
-      :user_ip => "127.0.0.1",
-      :article_date => Time.now,
-      :comment_author => "Henrik Hodne",
-      :comment_type => "comment",
-      :test_force => "ham,0.1000"
-    ).spam?.should be_false
-  end
-  
-  it "should correctly set the spaminess" do
-    @defender.
-      stubs(:call_action).
-      with('audit-comment', {
-        "user-ip" => "127.0.0.1",
-        "article-date" => Time.now.strftime("%Y/%m/%d"),
-        "comment-author" => "Henrik Hodne",
-        "comment-type" => "comment",
-        "test-force" => "spam,0.5000",
-      }).
-      returns(
-      {"signature" => "abc123", "spam" => true, "spaminess" => 0.5}
-    )
-    @defender.audit_comment(
-      :user_ip => "127.0.0.1",
-      :article_date => Time.now,
-      :comment_author => "Henrik Hodne",
-      :comment_type => "comment",
-      :test_force => "spam,0.5000"
-    ).spaminess.should == 0.5
-  end
-  
-  it "should change IPv6-style IPv4 adresses to IPv4 adresses" do
-    @defender.
-      stubs(:call_action).
-      with('audit-comment', {
-        "user-ip" => "127.0.0.1",
-        "article-date" => Time.now.strftime("%Y/%m/%d"),
-        "comment-author" => "Henrik Hodne",
-        "comment-type" => "comment",
-        "test-force" => "spam,0.5000",
-      }).
-      returns(
-      {"signature" => "abc123", "spam" => true, "spaminess" => 0.5}
-    )
-    
-    @defender.audit_comment(
-      :user_ip => "::ffff:127.0.0.1",
-      :article_date => Time.now,
-      :comment_author => "Henrik Hodne",
-      :comment_type => "comment",
-      :test_force => "spam,0.5000"
-    ).spaminess.should == 0.5
-  end
-  
-  it "should fail without valid API credentials" do
-    @defender.
-      stubs(:call_action).
-      with('audit-comment', {
-        "user-ip" => "127.0.0.1",
-        "article-date" => Time.now.strftime("%Y/%m/%d"),
-        "comment-author" => "Henrik Hodne",
-        "comment-type" => "comment",
-        "test-force" => "ham,0.1000",
-      }).
-      raises(StandardError)
-    lambda {
-      d.audit_comment(
-        :user_ip => "127.0.0.1",
-        :article_date => Time.now,
-        :comment_author => "Henrik Hodne",
-        :comment_type => "comment",
-        :test_force => "ham,0.1000"
+describe Defender do
+  context "Users" do
+    it "returns false when given an invalid API key" do
+      Defender.stubs(:request).with(:get, "/2.0/users/foobar.yaml", nil).
+        returns([404,
+                {"status" => "failed", "message" => "API key not found",
+                  "api-version" => "2.0", "owner-url" => ""}]
       )
-    }.should raise_error(StandardError)
-  end
-  
-  it "should correctly call the report-false-positives action with one signature given" do
-    @defender.
-      stubs(:call_action).
-      with('report-false-positives', {
-        "signatures" => "1"
-      }).
-      returns(true)
-    @defender.report_false_positives(1).should == true
-  end
-  
-  it "should correctly call the report-false-positives action with one signature given (array)" do
-    @defender.
-      stubs(:call_action).
-      with('report-false-positives', {
-        "signatures" => "1"
-      }).
-      returns(true)
-    @defender.report_false_positives([1]).should == true
-  end
-  
-  it "should correctly call the report-false-positives action with multiple signature given" do
-    @defender.
-      stubs(:call_action).
-      with('report-false-positives', {
-        "signatures" => "1,2,3"
-      }).
-      returns(true)
-    @defender.report_false_positives([1,2,3]).should == true
+      Defender.api_key = "foobar"
+      Defender.check_api_key.should be_false
+    end
+
+    it "returns true when given a valid API key" do
+      Defender.stubs(:request).with(:get, "/2.0/users/barbaz.yaml", nil).
+        returns([200,
+                {"status" => "success", "message" => "",
+                  "api-version" => "2.0", "owner-url" => ""}]
+               )
+      Defender.api_key = "barbaz"
+      Defender.check_api_key.should be_true
+    end
   end
 
-  it "should correctly call the report-false-negatives action with one signature given" do
-    @defender.
-      stubs(:call_action).
-      with('report-false-negatives', {
-        "signatures" => "1"
-      }).
-      returns(true)
-    @defender.report_false_negatives(1).should == true
-  end
-  
-  it "should correctly call the report-false-negatives action with one signature given (array)" do
-    @defender.
-      stubs(:call_action).
-      with('report-false-negatives', {
-        "signatures" => "1"
-      }).
-      returns(true)
-    @defender.report_false_negatives([1]).should == true
-  end
-  
-  it "should correctly call the report-false-negatives action with multiple signature given" do
-    @defender.
-      stubs(:call_action).
-      with('report-false-negatives', {
-        "signatures" => "1,2,3"
-      }).
-      returns(true)
-    @defender.report_false_negatives([1,2,3])
+  context "Documents" do
+    it "should allow an innocent comment to be posted when given only required options" do
+      Defender.stubs(:request).with(:post, "/2.0/users/foobar/documents.yaml", {
+        "client" => "Defender | 0.2 | Henrik Hodne | henrik.hodne@binaryhex.com",
+        "content" => "[innocent,0.1]",
+        "platform" => "ruby",
+        "type" => "test"
+      }).returns([200,
+        {
+          "api-version" => "2.0",
+          "status" => "success",
+          "message" => "",
+          "signature" => "baz",
+          "allow" => true,
+          "classification" => "innocent",
+          "spaminess" => 0.1,
+          "profanity-match" => false
+        }])
+
+      Defender.api_key = "foobar"
+      document = Defender::Document.new
+      document.content = "[innocent,0.1]"
+      document.type = :test
+      document.save
+
+      document.allow?.should be_true
+      document.classification.should == "innocent"
+      document.spaminess.should == 0.1
+      document.profane?.should be_false
+      document.signature.should == "baz"
+    end
   end
 end
